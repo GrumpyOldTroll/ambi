5697c2461d320a98f1e063af9f341d8567c12d14
Max Franke <mfranke@inet.tu-berlin.de>
2019-11-18 17:26:27 +0800

    Works.... somewhat

--- ambiFramer.py
+++ ambiFramer.py
@@ -1,8 +1,14 @@
 import pytaps as taps
 import argparse
 import asyncio
-
+import ipaddress
+import struct
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from hasher import ManifestHasher
 color="blue"
+color2="red"
+
 
 class AmbiFramer(taps.Framer):
     def __init__(self, remote_endpoint, security_parameters=None, local_endpoint=None):
@@ -10,36 +16,92 @@ class AmbiFramer(taps.Framer):
         self.local_endpoint = local_endpoint
         self.preconnection = None
         self.connection = None
+        self.hasher = None
+        self.source = None
+        self.group = None
+        self.group_port = None
+        self.manifest_id = 15
+        self.hashes = []
+        self.in_mpp = 0
+        self.out_mpp = 0
     
     async def handle_received_partial(self, data, context, end_of_message,
                                       connection):
-        taps.print_time("Received partial message " + str(data) + ".", color)
+        #taps.print_time("Received manifest " + str(data) + ".", color2)
+        manifest = struct.unpack_from('IIIHH', data)
+        print("Manifest")
+        self.manifest_id = int(manifest[0])
+        hashes = data[16:]
+        len_hashes = int(len(hashes)/manifest[4])
+        while(len(hashes) > 0):
+            self.hashes.append(hashes[:len_hashes])
+            hashes = hashes[len_hashes:]
+
         await self.connection.receive(min_incomplete_length=1)
 
     async def handle_received(self, data, context, connection):
-        taps.print_time("Received message " + str(data) + ".", color)
+        taps.print_time("Received manifest " + str(data) + ".", color2)
         await self.connection.receive(min_incomplete_length=1)
     
     async def handle_ready(self, connection):
-        print("Connection ready")
-        msgref = await self.connection.receive()
+        taps.print_time("TCP connection ready", color2)
         self.connection.on_received_partial(self.handle_received_partial)
         self.connection.on_received(self.handle_received)
         await self.connection.receive(min_incomplete_length=1)
 
+    async def handle_received_data(self, connection):
+        self.hasher = ManifestHasher(self.source, self.group, self.group_port,hashes.SHAKE128(16),self.manifest_id)
+        self.in_mpp+=1
+        stream, context, eom = self.parse(connection, 0, 0)
+        if len(stream) == 0:
+            raise taps.DeframingFailed
+            return
+        timeout = 0
+        msg = stream[0]
+        hash = self.hasher.digest(msg, connection.remote_endpoint.port)
+        print("Hash")
+        print(hash)
+        while(timeout <= 10):
+            if hash in self.hashes:
+                    print("Hash found")
+                    self.hashes.remove(hash)
+                    self.out_mpp+=1
+                    taps.print_time("There have been " + str(self.in_mpp) + " incoming multicast packets while " + str(self.out_mpp) + " have been handed to the application.")
+                    return (None,msg,len(msg),True)
+            else:
+                await asyncio.sleep(1)
+                timeout+= 1
+        raise taps.DeframingFailed
+        return
+
     async def start(self, connection):
         
+        self.source = ipaddress.ip_address(connection.remote_endpoint.address[0])
+        self.group = ipaddress.ip_address(connection.local_endpoint.address[0])
+        self.group_port = connection.local_endpoint.port
         tp = taps.TransportProperties()
         self.preconnection = taps.Preconnection(remote_endpoint=self.remote_endpoint, local_endpoint=self.local_endpoint, transport_properties=tp)
         self.preconnection.on_ready(self.handle_ready)
         self.connection = await self.preconnection.initiate()
 
+
 class AmbiClient():
     def __init__(self, args):
         self.listener = None
         self.preconnection = None
+        self.connection = None
         self.loop = asyncio.get_event_loop()
         self.args = args
+
+    async def handle_received(self, data, context, connection):
+        taps.print_time("Received multicast message " + str(data) + ".", color)
+        await self.connection.receive()
+
+    async def handle_connection_received(self, connection):
+        self.connection = connection
+        self.connection.on_received(self.handle_received)
+        await self.connection.receive()
+
 
     async def main(self):
         mcg = taps.LocalEndpoint()
@@ -50,7 +112,7 @@ class AmbiClient():
         ssm.with_port(args.ssm_port)
 
         rambi = taps.RemoteEndpoint()
-        rambi.with_address([args.remote_ambi_address])
+        rambi.with_address(args.remote_ambi_address)
         rambi.with_port(args.remote_ambi_port)
         if args.local_ambi_address is not None:
             lambi = taps.LocalEndpoint()
@@ -68,11 +130,10 @@ class AmbiClient():
         self.preconnection = taps.Preconnection(remote_endpoint=ssm,
                                                 local_endpoint=mcg,
                                                 transport_properties=tp)
-        
+        self.preconnection.on_connection_received(self.handle_connection_received)
         ambi = AmbiFramer(remote_endpoint = rambi, local_endpoint= lambi)
         self.preconnection.add_framer(ambi)
         self.listener = await self.preconnection.listen()
-        print(self.listener.local_endpoint.port)
 
 if __name__ == "__main__":
     # Parse arguments
